11617e0624b8a02e82a55df8742ac4c15c4e2539
Aaron Imani <alirezi@uci.edu>
2024-06-15 21:11:33 -0700

    Fixed logger

---
+++ common/log_config.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+import colorlog
+
+log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
+
+
+def get_logger(name):
+    logger = colorlog.getLogger(name)
+    ch = colorlog.StreamHandler()
+    ch.setLevel(log_level)
+    formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(name)s: %(reset)s%(message)s",
+        log_colors={
+            "DEBUG": "green",
+            "INFO": "cyan",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+    ch.setFormatter(formatter)
+    ch.setLevel(log_level)
+    logger.setLevel(log_level)
+
+    if not logger.hasHandlers():
+        logger.addHandler(ch)
+
+    return logger
+
+
+colorlog.getLogger().setLevel(logging.ERROR)
